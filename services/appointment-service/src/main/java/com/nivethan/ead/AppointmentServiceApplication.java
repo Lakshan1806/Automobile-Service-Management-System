ceacb31dad452c09257794864dccd52e25381ccf
nivethan-nirosh <nirosh.26d@gmail.com>
2025-09-23 21:16:15 +0530

    refactor: Initializing Spring-Boot Project

--- services/appointment-service/src/main/java/com/nivethan/ead/AppointmentServiceApplication.java
+++ services/appointment-service/src/main/java/com/nivethan/ead/AppointmentServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppoinmentServiceApplication {
+public class AppointmentServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppoinmentServiceApplication.class, args);
+        SpringApplication.run(AppointmentServiceApplication.class, args);
     }
 
 }
