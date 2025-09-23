91d0de40e8c66f3d02d9abf5affad3dd8f8cfd63
nivethan-nirosh <nirosh.26d@gmail.com>
2025-09-23 21:06:43 +0530

    refactor: Initializing Spring-Boot Project

---
+++ services/appoinment-service/src/test/java/com/nivethan/ead/AppoinmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivethan.ead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoinmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
