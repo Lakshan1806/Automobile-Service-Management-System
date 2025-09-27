cc48fe6720312e26f167df04426b28c9078758e2
Lakshan1806 <lakshanuthayan@gmail.com>
2025-09-27 15:57:08 +0530

    feat: Add initial location route file
    Created the v1 locationRoute.js file with basic Express router setup and placeholders for future route handlers.

---
+++ services/location-service/src/routes/v1/locationRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import locationController from "../../controllers/locationController.js";
+
+const locationRoutes = express.Router();
+
+//locationRoutes.post("/", );
+//locationRoutes.get("/", );
+
+export default locationRoutes;
