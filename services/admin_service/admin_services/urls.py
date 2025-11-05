95e36565e2f4a2b5be9de5b469c762b45fb25d28
Vithushan Kanesamoorthy <vithuvithush44@gmail.com>
2025-11-06 00:07:47 +0530

    Feat:Add unit test cases for chatbot

---
+++ services/admin_service/admin_services/urls.py
@@ -0,0 +1,52 @@
+from django.urls import path
+from .views import (
+    EmployeeCreateView,
+    EmployeeUpdateView,
+    EmployeeDeleteView,
+    get_employee_role,
+    EmployeeListView,
+)
+from .views import (
+    BranchCreateView,
+    BranchListView,
+    BranchUpdateView,
+    BranchDeleteView,
+    AvailableManagersView
+)
+from .views import (
+    ServiceCreateView,
+    ServiceListView,
+    ServiceUpdateView,
+    ServiceDeleteView,
+)
+from .views import (
+    ProductCreateView,
+    ProductListView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
+
+
+urlpatterns = [
+    path("employees/create/", EmployeeCreateView.as_view(), name="create_employee"),
+    path("employees/all/", EmployeeListView.as_view(), name="create_employee"),
+    path("employees/<int:employee_id>/", EmployeeUpdateView.as_view(), name="update_employee"),
+    path("employees/<int:employee_id>/delete/", EmployeeDeleteView.as_view(), name="delete_employee"),
+    path("employees/role/", get_employee_role, name="get_employee_role"),
+
+    path('branches/create/', BranchCreateView.as_view(), name='branch-create'),
+    path('branches/', BranchListView.as_view(), name='branch-list'),
+    path('branches/<int:branch_id>/update/', BranchUpdateView.as_view(), name='branch-update'),
+    path('branches/<int:branch_id>/delete/', BranchDeleteView.as_view(), name='branch-delete'),
+    path('branches/managers/', AvailableManagersView.as_view(), name='available-managers'),
+
+    path("services/create/", ServiceCreateView.as_view(), name="service-create"),
+    path("services/", ServiceListView.as_view(), name="service-list"),
+    path("services/<int:service_id>/update/", ServiceUpdateView.as_view(), name="service-update"),
+    path("services/<int:service_id>/delete/", ServiceDeleteView.as_view(), name="service-delete"),
+
+    path('products/create/', ProductCreateView.as_view(), name='product-create'),
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:product_id>/update/', ProductUpdateView.as_view(), name='product-update'),
+    path('products/<int:product_id>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+]
