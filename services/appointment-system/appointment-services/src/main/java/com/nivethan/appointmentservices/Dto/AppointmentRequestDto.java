1aa3b0a41441812ea39be4f068e57153edc14a7c
nivethan-nirosh <nirosh.26d@gmail.com>
2025-11-05 21:03:24 +0530

    refactor: minor changes

--- services/appointment-system/appointment-services/src/main/java/com/nivethan/appointmentservices/Dto/AppointmentRequestDto.java
+++ services/appointment-system/appointment-services/src/main/java/com/nivethan/appointmentservices/Dto/AppointmentRequestDto.java
@@ -1,7 +1,6 @@
 package com.nivethan.appointmentservices.Dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
