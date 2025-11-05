843419d695bff0d51ff3ebf1c3eb3de281e91c45
chajeyraj <chajeyraj@gmail.com>
2025-11-05 17:03:08 +0530

    add Vehicles

--- services/customer-service/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ services/customer-service/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aacada807ccee28fd2320c7a7956079da9a7ca7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2125e352ac7e635a708aa2ab4b6c161ce3d37e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
